ac0a202bbc1ebec2f2d441d82d62bf3f6328e25e
23antinipermatasari <antini.permatasari@mhs.pelitabangsa.ac.id>
2022-06-12 14:06:50 +0700

    menambahkan file baru

---
+++ Lab8_php_database/footer.php
@@ -0,0 +1,10 @@
+<div class="row">
+        <div class="col col-sm-12">
+            <div class="font-weight-bold bg-primary mb-1">
+                <div class="card-body text-center">
+                    Antini permatasari - 312010095 - TI.20.B.1 <br>
+                    &copy; 2022 - Universitas Pelita Bangsa - Teknik Informatika
+                </div>
+            </div>
+        </div>
+    </div>
